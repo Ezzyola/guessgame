62225e1770b36182c7603ebbfc77019f083069d9
Olagunju Enoch <80332398+Ezzyola@users.noreply.github.com>
2021-07-02 11:41:41 +0100

    Add files via upload

---
+++ Guess game.py
@@ -0,0 +1,25 @@
+# this is a guess the number game
+import random
+
+print('Hello, What is your name')
+name = input()
+
+print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
+secretNumber = random.randint(1, 20)
+
+for guessesTaken in range (1,7):
+    print('Take a guess.')
+    guess = int(input())
+
+    if guess < secretNumber:
+      print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else:
+        break
+
+if guess == secretNumber:
+    print('Good job you got my guesses in ' + str(guessesTaken) + 'guesses')
+else:
+    print('Nope the number i was thinking of was ' + str(secretNumber))
+    
